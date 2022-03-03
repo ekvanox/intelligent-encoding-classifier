8b350e876eed083858086e3760b7e8f6d68f65e9
ekvanox <erik@heimdal.dev>
2022-03-03 15:49:26 +0100

    chore: :adhesive_bandage: backup

--- intelligent-encoding-classifier/statistics.py
+++ intelligent-encoding-classifier/statistics.py
@@ -1,22 +1,24 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+from tensorflow import keras
 import random
 from tqdm import tqdm
 import numpy as np
+from tensorflow.keras.utils import to_categorical
+from numpy import array
 from sys import path
 
 # Disable tensorflow debug logs
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-from tensorflow import keras
-
 
 # Define constants
 CHARACTERS_IN_DATASET = (
-    "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM="
+    "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM= "
 )
 TEST_DATA_PATH: str = os.path.join("datasets", "test_data")
-TENSORFLOW_MODEL_PATH: str = os.path.join("models", "tensorflow_model.h5")
+TENSORFLOW_MODEL_PATH: str = os.path.join(
+    "models", "tensorflow_model_latest.h5")
 LEAVE_TIME_STATISTICS: bool = False
 
 # Set working directory to script directory
@@ -27,20 +29,15 @@ CLASS_NAMES = [directory for directory in os.listdir(TEST_DATA_PATH)]
 
 
 # Define functions
-def string_to_onehot(input_string):
-    return_list = []
+def string_to_onehot(input_string: str):
+    characters = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '0': 9, 'q': 10, 'w': 11, 'e': 12, 'r': 13, 't': 14, 'y': 15, 'u': 16, 'i': 17, 'o': 18, 'p': 19, 'a': 20, 's': 21, 'd': 22, 'f': 23, 'g': 24, 'h': 25, 'j': 26, 'k': 27, 'l': 28, 'z': 29, 'x': 30,
+                  'c': 31, 'v': 32, 'b': 33, 'n': 34, 'm': 35, 'Q': 36, 'W': 37, 'E': 38, 'R': 39, 'T': 40, 'Y': 41, 'U': 42, 'I': 43, 'O': 44, 'P': 45, 'A': 46, 'S': 47, 'D': 48, 'F': 49, 'G': 50, 'H': 51, 'J': 52, 'K': 53, 'L': 54, 'Z': 55, 'X': 56, 'C': 57, 'V': 58, 'B': 59, 'N': 60, 'M': 61, '=': 62, ' ': 63}
 
-    for char in input_string:
-        temp_onehot_list = [0] * len(CHARACTERS_IN_DATASET)
-        try:
-            temp_onehot_list[CHARACTERS_IN_DATASET.index(char)] = 1
-        except:
-            print(char)
-        return_list.append(temp_onehot_list)
+    input_string = input_string.ljust(64, ' ')
+    data = array([characters[c] for c in input_string])
+    onehot_list = to_categorical(data, num_classes=64)
 
-    for i in range(64 - len(return_list)):
-        return_list.append([0] * len(CHARACTERS_IN_DATASET))
-    return return_list
+    return onehot_list
 
 
 def load_test_data(_class):
@@ -61,7 +58,7 @@ for class_name in CLASS_NAMES:
     SAMPLE_STRINGS = load_test_data(class_name)
     SAMPLE_STRINGS = [
         sample_string for sample_string in SAMPLE_STRINGS if len(sample_string) < 63
-    ][:100000]
+    ][:100]
 
     # Define iterative counter variable
     correct = 0
@@ -70,11 +67,13 @@ for class_name in CLASS_NAMES:
     for prediction_string in tqdm(SAMPLE_STRINGS, desc=class_name, leave=LEAVE_TIME_STATISTICS):
         # Pre-process data
         INPUT_DATA = []
-        INPUT_DATA.append(string_to_onehot(prediction_string))
-        processed_input = np.array(INPUT_DATA).astype("float32")
+        INPUT_DATA.append(string_to_onehot(prediction_string).tolist())
+        # processed_input = np.array(INPUT_DATA).astype("float32")
 
         # Make predictions with model
         prediction_list = model.predict(INPUT_DATA)[0]
+
+        print(prediction_list)
 
         # Print the results
         correct += CLASS_NAMES[np.argmax(prediction_list)] == class_name
--- intelligent-encoding-classifier/trainer.py
+++ intelligent-encoding-classifier/trainer.py
@@ -1,13 +1,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+from tensorflow import keras
+from tqdm import tqdm
+import numpy as np
+from sys import path
+import tensorflow as tf
+from numpy import array
+from tensorflow.keras.utils import to_categorical
+import random
 
 # Disable tensorflow debug logs
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-from tensorflow import keras
-import numpy as np
-from sys import path
-import tensorflow as tf
 
 # Set working directory to script directory
 os.chdir(path[0])
@@ -39,26 +43,20 @@ def chunks(l, n):
     return [l[i: i + n] for i in range(0, len(l), n)]
 
 
-def string_to_onehot(input_string):
-    characters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM="
-    return_list = []
+def string_to_onehot(input_string: str):
+    characters = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '0': 9, 'q': 10, 'w': 11, 'e': 12, 'r': 13, 't': 14, 'y': 15, 'u': 16, 'i': 17, 'o': 18, 'p': 19, 'a': 20, 's': 21, 'd': 22, 'f': 23, 'g': 24, 'h': 25, 'j': 26, 'k': 27, 'l': 28, 'z': 29, 'x': 30,
+                  'c': 31, 'v': 32, 'b': 33, 'n': 34, 'm': 35, 'Q': 36, 'W': 37, 'E': 38, 'R': 39, 'T': 40, 'Y': 41, 'U': 42, 'I': 43, 'O': 44, 'P': 45, 'A': 46, 'S': 47, 'D': 48, 'F': 49, 'G': 50, 'H': 51, 'J': 52, 'K': 53, 'L': 54, 'Z': 55, 'X': 56, 'C': 57, 'V': 58, 'B': 59, 'N': 60, 'M': 61, '=': 62, ' ': 63}
 
-    for char in input_string:
-        temp_onehot_list = [0] * len(characters)
-        try:
-            temp_onehot_list[characters.index(char)] = 1
-        except:
-            print("Error: " + char)
-        return_list.append(temp_onehot_list)
+    input_string = input_string.ljust(64, ' ')
+    data = array([characters[c] for c in input_string])
+    onehot_list = to_categorical(data, num_classes=64)
 
-    for i in range(64 - len(return_list)):
-        return_list.append([0] * len(characters))
-    return return_list
+    return onehot_list
 
 
 model = keras.Sequential(
     [
-        keras.layers.Flatten(input_shape=(64, 63)),
+        keras.layers.Flatten(input_shape=(64, 64)),
         keras.layers.Dense(NEURONS, activation=keras.activations.relu),
         keras.layers.Dense(NEURONS, activation=keras.activations.relu),
         keras.layers.Dropout(DROPOUT),
@@ -73,12 +71,15 @@ training_data = []
 
 for i, class_name in enumerate(CLASS_NAMES):
     print(f"Loading class {class_name}")
-    training_data.append(chunks(load_test_data(class_name), 10_000))
+    training_data.append(load_test_data(class_name))
 
+random.shuffle(training_data)
+print(training_data)
+training_data = chunks(training_data, 100_000)
 
 # Load a user specified number inputs at a time
-for training_iteration in range(len(training_data[0])):
-    print(f"Iteration {training_iteration}")
+for training_iteration in tqdm(range(len(training_data[0]))):
+    # print(f"Iteration {training_iteration}/{len(training_data[0])}")
 
     INPUT_DATA = []
     INPUT_LABLES = []
@@ -87,6 +88,11 @@ for training_iteration in range(len(training_data[0])):
         for _string in encoding_data[training_iteration]:
             INPUT_DATA.append(string_to_onehot(_string))
         INPUT_LABLES += [i for _ in encoding_data[training_iteration]]
+        # print(i, end=',', flush=True)
+
+    print(training_data[0])
+
+    print(INPUT_LABLES)
 
     try:
         INPUT_LABLES = np.array(INPUT_LABLES).astype("float32")
@@ -95,17 +101,25 @@ for training_iteration in range(len(training_data[0])):
         print(e)
         continue
 
-    model.fit(
+    training_history = model.fit(
         INPUT_DATA,
         INPUT_LABLES,
         epochs=EPOCHS,
         batch_size=BATCH_SIZE,
         validation_split=0.1,
-        verbose=1,
-        shuffle=True,
+        verbose=0,
+        shuffle=False,
         use_multiprocessing=True,
     )
 
+    # Make predictions with model
+    prediction_list = model.predict(INPUT_DATA[:1])[0]
+
+    # Print the results
+    print(CLASS_NAMES[np.argmax(prediction_list)], INPUT_LABLES[0])
+
+loss = training_history.history['val_loss']
+
 # Save model
-model.save(os.path.join(TENSORFLOW_MODELS_PATH, f"tensorflow_model-{NAME}.h5"))
+model.save(os.path.join(TENSORFLOW_MODELS_PATH, f"{loss}-{NAME}.h5"))
 model.save(os.path.join(TENSORFLOW_MODELS_PATH, "tensorflow_model_latest.h5"))
